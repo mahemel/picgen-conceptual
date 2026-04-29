fddbcf9e56b5bfc306cd0e442d3f1db0acfbf31e
mahemel <mahbubhasanhemel@gmail.com>
2026-04-29 20:47:14 +0600

    photo detail page added

--- src/app/(main)/all-photos/[id]/page.jsx
+++ src/app/(main)/all-photos/[id]/page.jsx
@@ -1,10 +1,18 @@
+import PhotoCard from "@/components/PhotoCard";
+import { getPhotos } from "@/lib/data";
 import React from "react";
 
 const PhotosDetailPage = async ({ params }) => {
     const { id } = await params;
+
+    const photos = await getPhotos();
+
+    const photo = photos.find((photo) => photo.id === parseInt(id));
+
     return (
         <div>
             <h2>Photos Detail {id}</h2>
+            <PhotoCard photo={photo}></PhotoCard>
         </div>
     );
 };
